88e2894acc581fc17de815b8ae47a77f56e3d9e4
DNeosphere <daniela.gomezp05@outlook.com>
2019-06-20 20:36:22 +0000

    if with &&

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,9 +9,9 @@
 int main(void)
 {
 char letter;
- for (letter = 'a'; letter <=  'z'; letter++)
+for (letter = 'a'; letter <=  'z'; letter++)
 {
-if (letter != 'e' || letter != 'q')
+if (letter != 'e' && letter != 'q')
 {
 putchar(letter);
 }
