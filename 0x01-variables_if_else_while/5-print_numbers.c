4135f7e4fd07333e8dd41f6f5e5277ee5bc184fa
DNeosphere <daniela.gomezp05@outlook.com>
2019-06-20 20:42:14 +0000

    task 5, show numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+n = 0;
+while (n <=  9)
+{
+putchar(n);
+n++;
+}
+putchar('\n');
+return (0);
+}
