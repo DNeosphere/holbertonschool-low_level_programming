e7ef07f8e213df9884770a8618fdc4356046e41c
DNeosphere <daniela.gomezp05@outlook.com>
2019-06-20 21:34:14 +0000

    alphabet reversed

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int codeA;
+for (codeA = 48; codeA <= 57; codeA++)
+{
+putchar(codeA);
+}
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ralpha;
+for (ralpha = 'z'; ralpha >= 'a'; ralpha--)
+{
+putchar(ralpha);
+}
+putchar('\n');
+ return (0);
+}
