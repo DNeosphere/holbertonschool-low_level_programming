5d9a655f4ab216eaac13e38d61411f416eed9829
DNeosphere <daniela.gomezp05@outlook.com>
2019-06-20 19:28:37 +0000

    zero changed for 0 in 1 task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ printf("Last digit of %d is %d and is greater than 5\n", n, lastn);
 }
 else if (lastn == 0)
 {
-printf("Last digit of %d is %d and is zero\n", n, lastn);
+printf("Last digit of %d is %d and is 0\n", n, lastn);
 }
 else if (lastn < 6 && !0)
 {
