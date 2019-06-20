2dbb7134c4ce6d339763bff2dbf81ff4162eb026
DNeosphere <daniela.gomezp05@outlook.com>
2019-06-20 19:22:26 +0000

    while loop and if loop with mod

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char letter;
+letter = 'a';
+while (letter <=  'z')
+{
+putchar(letter);
+letter++;
+}
+putchar('\n');
+return (0);
+}
