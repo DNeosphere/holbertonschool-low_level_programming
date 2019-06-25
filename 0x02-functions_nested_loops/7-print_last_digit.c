9b9f8082b6d3bcd9ede771e1e6ea7dc29cf61b3e
DNeosphere <daniela.gomezp05@outlook.com>
2019-06-25 14:00:48 +0000

    one condition

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,16 +12,11 @@ int nn;
 int pn;
 nn = -1;
 n10 = 10;
-if (n > 0)
+if (n >= 0)
 {
 mod = n % n10;
 _putchar (mod + '0');
 return (mod);
-}
-else if (n == 0)
-{
-_putchar (n + '0');
-return (n);
 }
 else
 {
