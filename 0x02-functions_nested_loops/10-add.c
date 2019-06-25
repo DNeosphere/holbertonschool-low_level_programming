feb313cc46652708e528e10c7b7cc80e9e5cbeda
DNeosphere <daniela.gomezp05@outlook.com>
2019-06-25 15:57:25 +0000

    addition of two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * add - adds two integers.
+ * @a:  first number
+ * @b: second number
+ * Return: result of addition
+ */
+int add(int a, int b)
+{
+int result;
+result = a + b;
+return (result);
+}
