70ae8d191799aa43ca8ec6d49eae1817cd90b559
DNeosphere <daniela.gomezp05@outlook.com>
2019-06-25 19:51:54 +0000

    betty is happy with task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,42 +1,40 @@
 #include "holberton.h"
 /**
  * times_table - matrix for table 9.
- * 
+ *
  */
 void times_table(void)
 {
-  int multr;
-  int multd;
-  for (multr = 0; multr <= 9; multr++)
-    {
-      for (multd = 0; multd <=9; multd++)
-	{
-	  if (multr * multd >= 10)
-	  {
-	    _putchar((multr * multd) / 10 + '0');
-	    _putchar((multr * multd) % 10 + '0');
-		if (multd != 9)
-		{	    
-		_putchar(' ');
-	        _putchar(',');   
-		}
-	  }	
-	    
-      else 
-	    {	
-		if (multd != 0)
-		{	      
-	         _putchar(' ');
-		}
-	      _putchar((multr * multd) % 10 + '0');
-		if (multd != 9)	     
-		{
-		 _putchar(',');
-	        _putchar(' ');
-	        } 
-	    }	
-	}
-      _putchar('\n');
-    }
-
+int multr;
+int multd;
+for (multr = 0; multr <= 9; multr++)
+{
+for (multd = 0; multd <= 9; multd++)
+{
+if (multr * multd >= 10)
+{
+_putchar((multr * multd) / 10 + '0');
+_putchar((multr * multd) % 10 + '0');
+if (multd != 9)
+{
+_putchar(' ');
+_putchar(',');
+}
+}
+else
+{
+if (multd != 0)
+{
+_putchar(' ');
+}
+_putchar((multr * multd) % 10 + '0');
+if (multd != 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+}
+_putchar('\n');
+}
 }
