eb0de1a5961a5d340e3028b885f8d3a0fc7b4bdb
DNeosphere <daniela.gomezp05@outlook.com>
2019-06-25 13:44:51 +0000

    alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 {
 int tentimes;
 char alpha;
-for (tentimes = 0; tentimes <= 10; tentimes++)
+for (tentimes = 0; tentimes < 10; tentimes++)
 {
 for (alpha = 'a'; alpha <= 'z'; alpha++)
 {
