66d89041674eedc1d2b7289cd1e92a2f1a7aa1bb
DNeosphere <daniela.gomezp05@outlook.com>
2019-07-04 21:55:13 +0000

    replace lowcase for uppercase, betty ok

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ * cap_string - replaces a lowercase word in a string after signs or spaces
+ * @s: pointer to the string
+ * Return: the string
+ */
+char *cap_string(char *s)
+{
+int a;
+int flag;
+flag = 0;
+	for (a = 0; s[a] != '\0'; a++)
+	{
+	if (s[a] == ',' || s[a] == ';' || s[a] == '.' || s[a] == '!' ||
+		s[a] == '?' || s[a] == '"' ||
+		s[a] == '(' || s[a] == ')' || s[a] == '{' || s[a] == '}' ||
+		s[a] == ' ' || s[a] == '\t' || s[a] == '\n')
+		{
+		flag = 1;
+		}
+	else if (flag == 1 && s[a] >= 97 && s[a] <= 122)
+		{
+		s[a] -= 32;
+		flag = 0;
+		}
+	else
+		{
+		flag = 0;
+		}
+	}
+return (s);
+}
