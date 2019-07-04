32647786b0a3dd2bfab78ec85902a78459c8d732
DNeosphere <daniela.gomezp05@outlook.com>
2019-07-04 20:54:38 +0000

    compare strings with pointers

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,6 +8,15 @@
 int _strcmp(char *s1, char *s2)
 {
 int minus;
-minus = *s1 - *s2;
-return (minus);
+while (*s1 != '\0' && *s2 != '\0')
+	{
+	if (*s1 != *s2)
+	{
+	minus = *s1 - *s2;
+	return (minus);
+	}
+	s1++;
+	s2++;	
+	}
+return (0);	
 }
