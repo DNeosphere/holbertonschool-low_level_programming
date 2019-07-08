8ee165a4a7a183e684607e859684f0b307f01643
DNeosphere <daniela.gomezp05@outlook.com>
2019-07-08 18:57:41 +0000

    2 task, checker not working, betty OK

---
+++ 0x07-pointers_arrays_strings/2-main.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *s = "hello";
+    char *f;
+    f = _strchr(s, 'z');
+    printf("%s\n", f);
+    return (0);
+}
---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _strchr - looks for the first occurence of a char
+ * @s: source string
+ * @c: character to find
+ * Return: Null if character not found or pointer to the C character
+ */
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{
+	if (*s == c)
+	break;
+	s++;
+}
+return (s);
+}
