678753f402c80ef0cf72f0fcf30575fccc4491ea
DNeosphere <daniela.gomezp05@outlook.com>
2019-07-09 05:00:11 +0000

    asd

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,19 +2,22 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * print_diagsums -  prints the sum of the two diagonals of
+ * a square matrix of integers
+ * @a: pointer
+ * @size: size of the matrix
  */
 void print_diagsums(int *a, int size)
 {
+
 int ciclo, sum1, sum2;
+
 sum1 = 0;
 sum2 = 0;
 	for (ciclo = 0; ciclo < size; ciclo++)
 	{
-	sum1 += *(a + (size * ciclo + ciclo));
-	sum2 += *(a + (size * ciclo + size - 1 - ciclo));
+		sum1 += *(a + (size * ciclo + ciclo));
+		sum2 += *(a + (size * ciclo + size - 1 - ciclo));
 	}
 printf("%d, ", sum1);
 printf("%d\n", sum2);
--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,5 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 void print_chessboard(char (*a)[8]);
 int _putchar(char c);
