72906bd3321654fc81bbae2eb70f0b6e6e48032b
DNeosphere <daniela.gomezp05@outlook.com>
2019-07-09 02:30:12 +0000

    betty ok

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 /**
- * _strspn - gets the length of a prefix substring.
- * @s: string to he searched
- * @accept: prefix substring
- * Return: number of bytes in the initial segment of s which
- * consist only of bytes from accept
+ * _strstr - locates a substring.
+ * @h: string to he searched
+ * @n: prefix substring
+ * Return:  a pointer to the beginning of the located substring,
+ * NULL if the substring is not found.
  */
 char *_strstr(char *h, char *n)
 {
@@ -12,13 +12,13 @@ int a, b, c;
 a = 0;
 	while (h[a] != '\0')
 	{
-	b = 0;
-	c = a;
-		while (h[a] == n[b] && h[a] != '\0'&& n[b] != '\0')
-			{
+		b = 0;
+		c = a;
+		while (h[a] == n[b] && h[a] != '\0' && n[b] != '\0')
+		{
 				a++;
 				b++;
-			}
+		}
 		if (n[b] == '\0')
 			return (h + c);
 		a = c + 1;
