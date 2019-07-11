93495f5905074e394a00ba392dfd981eef26b5b2
DNeosphere <daniela.gomezp05@outlook.com>
2019-07-11 03:09:30 +0000

    logic improved, betty corrected

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,21 +1,27 @@
 #include "holberton.h"
 /**
- * _sqrt_recursion - prints the nature square root of a number
+ * is_prime_number - checks whether a number is prime or not
  * @n: the number
- * Return: aaa
+ * Return: 1 if prime, 0 if not
  */
 int is_prime_number(int n)
 {
-	if (n <= 2)
+	if (n <=  1)
 		return (0);
-	return (fprime(n , 1));
+	return (fprime(n, 2));
 }
+/**
+ * fprime - checks whether a number is prime or not
+ * @n: given number
+ * @x: iterator to divide
+ * Return: 1 if prime, cero if not
+ */
 
-int fprime (int n, int x)
+int fprime(int n, int x)
 {
-	if (x == n - 1)
+	if ((n != x) && (n % x == 0))
+		return (0);
+	else if (n == x)
 		return (1);
-	else if (n % n - x)
-		return (fprime(n, x + 1));
-	return (0);
+	return (fprime(n, x + 1));
 }
