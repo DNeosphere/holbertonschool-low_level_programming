088ad122d3811da375b37066251568263febc140
DNeosphere <daniela.gomezp05@outlook.com>
2019-07-12 00:18:26 +0000

    task 7

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -1,5 +1,7 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+int is_palindrome(char *s);
+int fx3pali(char *s, char *p);
 int is_prime_number(int n);
 int fprime (int n, int x);
 int _sqrt_recursion(int n);
