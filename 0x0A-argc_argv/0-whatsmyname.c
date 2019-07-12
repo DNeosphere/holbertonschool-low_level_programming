ba782d7a8614a25cf696f94ee22383d13a365c14
DNeosphere <daniela.gomezp05@outlook.com>
2019-07-12 20:25:21 +0000

    task 0 argc, argv project

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the executable file with argc, argv
+ * @argc: number of arguments of the command line
+ * @argv: array of strings coitaining the arguments.
+ * Return: 0
+ */
+int main (int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - argc]);
+return (0);
+}
