62b1bcad9047583c36647aa2696a18ae426c65b5
DNeosphere <daniela.gomezp05@outlook.com>
2019-07-13 18:46:31 +0000

    task 3, betty ok

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the name of the executable file with argc, argv
+ * @argc: number of arguments of the command line
+ * @argv: array of strings coitaining the arguments.
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+	int mul = 1;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		for (a = 1; a < argc; a++)
+		{
+			mul *= atoi(argv[a]);
+		}
+	printf("%d\n", mul);
+	}
+return (0);
+}
