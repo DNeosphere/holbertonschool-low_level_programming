6eef9339e3d1320bd52cb3759b5ab48af35d42aa
DNeosphere <daniela.gomezp05@outlook.com>
2019-07-13 17:50:58 +0000

    betty happy with task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: array of strings coitaining the arguments.
  * Return: 0
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[argc - argc]);
 return (0);
