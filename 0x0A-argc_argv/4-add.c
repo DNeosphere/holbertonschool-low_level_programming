144f0df67920c2ee7b0429c710641ef459e9a00d
DNeosphere <daniela.gomezp05@outlook.com>
2019-07-13 20:29:02 +0000

    task 4, betty ok

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the name of the executable file with argc, argv
+ * @argc: number of arguments of the command line
+ * @argv: array of strings coitaining the arguments.
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, sum = 0;
+
+	for (a = 1; a < argc; a++)
+	{
+		for (b = 0; *(*(argv + a) + b) != '\0' ; b++)
+		{
+			if (*(*(argv + a) + b) < 48 || *(*(argv + a) + b) > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	sum += atoi(*(argv + a));
+	}
+	printf("%d\n", sum);
+	return (0);
+}
