8143249efd2bb1cda60da063fa157caaae91a415
DNeosphere <daniela.gomezp05@outlook.com>
2019-07-13 18:23:09 +0000

    task 1 and 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -6,8 +6,12 @@
  * @argv: array of strings coitaining the arguments.
  * Return: 0
  */
-int main(int argc, char __attribute__((unused)) *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%d\n", argc - 1);
+int a;
+	for (a = 0; a < argc; a++)
+	{
+		printf("%s\n", argv[a]);
+	}
 return (0);
 }
