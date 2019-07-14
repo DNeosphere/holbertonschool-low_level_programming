a3f681c602bb600e65e31bf8c067fecdf983b52d
DNeosphere <daniela.gomezp05@outlook.com>
2019-07-14 17:39:33 +0000

    100 task, converts money to coins

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * coins - transforms a number into coins (25, 10, 5, 2, 1)
+ * @x: number to trasnform
+ * Return: the amount of coins needed to express that number
+ */
+int coins(int x);
+/**
+ * main - transforms a number into cent coins
+ * @argc: number of arguments of the command line
+ * @argv: array of strings coitaining the arguments.
+ * Return: 1 if argc < 2, 0 if the value of argc is negative
+ * or the amount of coins needed to express a number
+ */
+int main(int argc, char *argv[])
+{
+	int money;
+
+	if (argc != 2)
+	{
+		puts("Error");
+		return (1);
+	}
+	money = atoi(argv[1]);
+	if (money < 0)
+	{
+		puts("0");
+	}
+	else
+	{
+		printf("%d\n", coins(money));
+		return (0);
+	}
+}
+/**
+ * coins - transforms a number into coins (25, 10, 5, 2, 1)
+ * @x: number to trasnform
+ * Return: the amount of coins needed to express that number
+ */
+int coins(int x)
+{
+	if (x == 0)
+		return (0);
+	if (x % 25 == 0)
+		return (coins(x - 25) + 1);
+	else if (x % 10 == 0)
+		return (coins(x - 10) + 1);
+	else if (x % 5 == 0)
+		return (coins(x - 5) + 1);
+	else if (x % 2 == 0)
+		return (coins(x - 2) + 1);
+	else if (x % 1 == 0)
+		return (coins(x - 1) + 1);
+}
