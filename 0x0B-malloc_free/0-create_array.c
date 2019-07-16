5a7b6fa1bf28d42f70cdaf5fcaeb320bdf757d09
DNeosphere <daniela.gomezp05@outlook.com>
2019-07-16 17:07:34 +0000

    task 0, malloc into a pointer to char

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * create_array - creates an array of chars with malloc
+ * @c: character to fill the array
+ * @size: amount of memory slots to reserve
+ * Return: NULL if size == 0, a pointer to the array, null if
+ * the memory allocation fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int a;
+	char *t;
+	t = /*(char *)*/ malloc(sizeof(c) * size);
+
+	if (t == NULL)
+		return (NULL);
+	if (size == 0)
+		return (NULL);
+	for (a = 0; a <= size; a++)
+	{
+		t[a] = c;
+	}
+return (t);
+}
