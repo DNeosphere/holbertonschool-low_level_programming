9a6859314575c952a6026ba7f0d2412533664e8c
DNeosphere <daniela.gomezp05@outlook.com>
2019-07-16 20:08:38 +0000

    task 2, betty ok

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * str_concat- concatenates two strings into an allocated space of memory
+ * @s1: string 1
+ * @s2: string 2
+ * Return: null if the memory allocation fails
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int len1, len2, a, b, copya, copyb;
+	char *strcon;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (a = 0; s1[a] != '\0'; a++)
+	{
+		len1++;
+	}
+	for (b = 0; s2[b] != '\0'; b++)
+	{
+		len2++;
+	}
+	len2 += 1;
+	strcon = (char *) malloc(sizeof(char) * (len1 + len2));
+	if (strcon == NULL)
+		return (NULL);
+	for (copya = 0; copya <= len1; copya++)
+	{
+		strcon[copya] = s1[copya];
+	}
+	for (copyb = 0; copyb <= len2; copyb++)
+	{
+		strcon[len1 + copyb] = s2[copyb];
+	}
+	return (strcon);
+}
