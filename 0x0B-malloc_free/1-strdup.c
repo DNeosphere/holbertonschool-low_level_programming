ac3ba0d20265189db2ddebb8470e710b2f6452dd
DNeosphere <daniela.gomezp05@outlook.com>
2019-07-16 18:16:33 +0000

    task 1, betty ok.

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _strdup - copies a string into a newly allocated space in memory
+ * @str: string to copy
+ * Return: NULL if size == 0, a pointer to the array, null if
+ * the memory allocation fails
+ */
+char *_strdup(char *str)
+{
+	char *news;
+	int a, b, len = 0;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		len++;
+	}
+	news = malloc(sizeof(char) * len);
+	if (str == NULL)
+		return (NULL);
+	if (news == NULL)
+		return (NULL);
+	for (b = 0; b <= len; b++)
+	{
+		news[b] = str[b];
+	}
+	return (news);
+}
