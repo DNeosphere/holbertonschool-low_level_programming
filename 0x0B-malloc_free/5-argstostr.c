7bfd1943fa2acb66da334c612fcdd9601e095d83
DNeosphere <daniela.gomezp05@outlook.com>
2019-07-17 00:34:43 +0000

    task 5, betty happy

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * argstostr - copies the arguments from argv to a single string
+ * @ac: count of arguments
+ * @av: pointer to strings
+ * Return: NULL if size == 0, a pointer to the array, null if
+ * the memory allocation fails
+ */
+char *argstostr(int ac, char **av)
+{
+	int a, b, c, d, cont = 0;
+	char *str, *str2;
+
+	for (a = 0; a < ac; a++)
+	{
+		for (b = 0; av[a][b] != '\0'; b++)
+		{
+			cont++;
+		}
+	cont++;
+	}
+	cont += 1;
+	str = malloc(sizeof(char) * cont);
+	str2 = str;
+	for (c = 0; c < ac; c++)
+	{
+		for (d = 0; av[c][d] != '\0'; d++)
+		{
+			*str = av[c][d];
+			str++;
+		}
+	*str = '\n';
+	str++;
+	}
+return (str2);
+}
