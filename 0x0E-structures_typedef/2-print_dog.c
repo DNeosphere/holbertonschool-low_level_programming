7da414f9f65b1e5ab9c3c34278690dc6a537bb0e
DNeosphere <daniela.gomezp05@outlook.com>
2019-07-22 17:48:37 +0000

    task 2, a float can not be null

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,7 +1,21 @@
-#include "dog.h"
 #include <stdio.h>
-
 #include "dog.h"
-#include <stdio.h>
+/**
+ * print_dog - prints the members of a structure, dog.
+ * @d: pointer to the structure
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
 
-
+}
