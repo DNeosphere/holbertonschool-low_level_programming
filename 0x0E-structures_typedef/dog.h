7a44e042e45471cad0353ab6f4cb912100d97c76
DNeosphere <daniela.gomezp05@outlook.com>
2019-07-22 17:49:51 +0000

    task 2, a float can not be null

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-int _putchar(char c);
 /**
  * struct dog - info about cute doggies
  * @name: of doggies
@@ -15,5 +14,7 @@ struct dog
 	float age;
 	char *owner;
 };
+void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
+int _putchar(char c);
 #endif
