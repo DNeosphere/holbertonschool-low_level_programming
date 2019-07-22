147f7c54a23d44edef38146bd64006f1f5da771b
DNeosphere <daniela.gomezp05@outlook.com>
2019-07-22 20:18:47 +0000

    struct filled at the end, first the copies

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,18 +16,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	ndog = malloc(sizeof(dog_t));
 	if (ndog == NULL)
 		return (NULL);
-	ndog->name = name;
-	ndog->age = age;
-	ndog->owner = owner;
-
-	cpname = _strdup(ndog->name);
+	cpname = _strdup(name);
 	if (cpname == NULL)
 	{
 		free(cpname);
 		free(ndog);
 		return (NULL);
 	}
-	cpowner = _strdup(ndog->owner);
+	cpowner = _strdup(owner);
 	if (cpowner == NULL)
 	{
 		free(cpowner);
@@ -35,6 +31,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(ndog);
 		return (NULL);
 	}
+	ndog->name = cpname;
+	ndog->age = age;
+	ndog->owner = owner;
 return (ndog);
 }
 /**
