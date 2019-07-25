72ee24afc90d1a5a3cc8e997b78e60777ff425b0
DNeosphere <daniela.gomezp05@outlook.com>
2019-07-25 01:07:03 +0000

    task 100, about upcode of a main function

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the opcodes of its own main function.
+ * @argc: arguments counter
+ * @argv: arguments values
+ *
+ * Return: result
+ */
+int main(int argc, char *argv[])
+{
+	int byamount;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (byamount < 0)
+	{
+	printf("Error\n");
+	exit(2);
+	}
+
+
+
+	return (0);
+
+}
