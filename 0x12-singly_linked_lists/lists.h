b60589e70366d539fd86426391419686fc1dbce4
DNeosphere <daniela.gomezp05@outlook.com>
2019-08-01 21:44:19 +0000

    free memory allocation from a list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -23,4 +23,5 @@ size_t list_len(const list_t *h);
 int _strlen(const char *s);
 list_t *add_node_end(list_t **head, const char *str);
 list_t *add_node(list_t **head, const char *str);
+void free_list(list_t *head);
 #endif /* LISTS_H */
