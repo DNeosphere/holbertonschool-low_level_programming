546da0795d3b89e825918b3cc33a383b246d895c
DNeosphere <daniela.gomezp05@outlook.com>
2019-08-01 22:29:13 +0000

    advanced task: run a function before main with constructor macro

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -18,6 +18,7 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+void liebre_tortuga(void) __attribute__ ((constructor));
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 int _strlen(const char *s);
