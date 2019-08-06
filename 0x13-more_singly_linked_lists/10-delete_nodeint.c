86ef6e9af5299b3a2c08da4175fa2075fb4ad73f
DNeosphere <daniela.gomezp05@outlook.com>
2019-08-06 19:12:58 +0000

    conditions changed, just one while

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,27 +12,43 @@ int delete_nodeint_at_index(listint_t **head, unsigned int idx)
 
 	if (head == NULL)
 		return (-1);
-
-	temp = *head;
-	count = 0;
-	while (count < idx)
+	if (idx == 0)
 	{
-		count++;
-		temp = temp->next;
-		if (temp == NULL)
-			return (-1);
+		pop_listint(head);
+		return (1);
 	}
-	temp_addr = temp->next;
-	free(temp);
+
 	temp = *head;
 	count = 0;
 	while (count < (idx - 1))
 	{
 		count++;
 		temp = temp->next;
-		if (temp == NULL)
+		if (temp == NULL|| temp->next == NULL)
 			return (-1);
 	}
+	temp_addr = temp->next->next;
+	free(temp->next);
 	temp->next = temp_addr;
 	return (1);
+
+}
+/**
+ * pop_listint -  deletes the head node of a listint_t linked list
+ * @head: pointer to the first node
+ * Return:  the head nodes data (n).
+ **/
+
+int pop_listint(listint_t **head)
+{
+	int copy;
+	listint_t *temp;
+
+	if (*head == NULL)
+		return (0);
+	temp = *head;
+	*head = (*head)->next;
+	copy = temp->n;
+	free(temp);
+	return (copy);
 }
