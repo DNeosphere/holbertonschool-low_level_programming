44d9515ded39cd8d46d4584f9ecaa54c56a733cf
DNeosphere <daniela.gomezp05@outlook.com>
2019-08-08 21:31:29 +0000

    task to print boinary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * print_binary - prints a binary from a decimal
+ * @n: number to print
+ **/
+void print_binary(unsigned long int n)
+{
+	int index_print, len, number;
+
+	len = 0;
+	while (n >> len)
+		len++;
+
+	/*print the number*/
+	for (index_print = len - 1; index_print >= 0; index_print--)
+	{
+		number = n >> index_print;
+		if (number & 1)
+			_putchar('1');
+		else
+			_putchar('0');
+	}
+}
--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -4,5 +4,6 @@
 unsigned int binary_to_uint(const char *b);
 int _strlen(const char *s);
 int _pow_recursion(int x, int y);
-
+void print_binary(unsigned long int n);
+int _putchar(char c);
 #endif /* HOLBERTON_H */
