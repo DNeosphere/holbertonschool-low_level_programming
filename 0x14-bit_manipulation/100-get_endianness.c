ff76d6055ec51f9ce1ddc63810e9487dbe22cc59
DNeosphere <daniela.gomezp05@outlook.com>
2019-08-09 01:14:56 +0000

    betty ok

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,11 +1,12 @@
 #include "holberton.h"
 /**
- * get_endianness - finds whether the machine is big or little endian * Return: 1 if little endian, 0 if big endian
+ * get_endianness - finds whether the machine is big or little endian
+ * Return: 1 if little endian, 0 if big endian
  **/
 int get_endianness(void)
 {
 	unsigned int num_check = 1;
-	char *ptr = (char*)&num_check; /*gets the address of the number*/
+	char *ptr = (char *)&num_check; /*gets the address of the number*/
 
 	if (*ptr)
 		return (1);
