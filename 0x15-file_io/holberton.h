e4a3fbfe9f466fc3f3396d2a04491ad80d6c2c06
DNeosphere <daniela.gomezp05@outlook.com>
2019-08-13 19:27:06 +0000

    task 0. Open, read, write

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,13 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+
+
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif /* HOLBERTON_H */
