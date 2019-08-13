44e6fb87882c791b0f487513e09b1d076da04059
DNeosphere <daniela.gomezp05@outlook.com>
2019-08-13 20:32:21 +0000

    Create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ * create_file - creates a file
+ * @filename: name of the file
+ * @file_content: content of the file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t size_write;
+	int file_d, len;
+
+	if (filename == NULL)
+		return (-1);
+	file_d = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
+	if (file_d == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		close(file_d);
+		return (1);
+	}
+	for (len = 0; text_content[len] != '\0'; len++)
+		;
+
+	size_write = write(file_d, text_content, len);
+	if (size_write ==  -1)
+	{
+		write(STDOUT_FILENO, "fails", 5);
+		return (-1);
+	}
+	close(file_d);
+	return (1);
+}
