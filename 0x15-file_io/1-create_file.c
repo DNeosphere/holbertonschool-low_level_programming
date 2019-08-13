0eafc1571960105265323bdcea1dda02c2e91e19
DNeosphere <daniela.gomezp05@outlook.com>
2019-08-13 20:35:51 +0000

    Betty ok, task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,7 +2,7 @@
 /**
  * create_file - creates a file
  * @filename: name of the file
- * @file_content: content of the file
+ * @text_content: content of the file
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
