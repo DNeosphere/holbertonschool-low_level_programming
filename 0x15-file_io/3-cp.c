83dd6e6b6fe556cedf27f3cfafd748198f275c62
DNeosphere <daniela.gomezp05@outlook.com>
2019-08-13 23:15:14 +0000

    betty ok

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,44 +11,37 @@ int main(int argc, char *argv[])
 	char buff[1024];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	{dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s", argv[1]);
+	{dprintf(STDERR_FILENO, "Error: Can't read from file %s", argv[1]);
 		exit(98);
 	}
 	size_read = read(file_from, buff, 1024);
 	if (size_read == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	{dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	file_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	if (file_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to%s\n",argv[2]);
+	{printf(STDERR_FILENO, "Error: Can't write to%s\n", argv[2]);
 		exit(99);
 	}
 	size_write = write(file_to, buff, size_read);
 	if (size_write == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to%s\n", argv[2]);
+	{dprintf(STDERR_FILENO, "Error: Can't write to%s\n", argv[2]);
 		exit(99);
 	}
 	clfrom = close(file_from);
 	if (clfrom == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd%d\n", file_from);
+	{dprintf(STDERR_FILENO, "Error: Can't close fd%d\n", file_from);
 		exit(100);
 	}
 	clto = close(file_to);
 	if (clto == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd%d\n", file_to);
+	{dprintf(STDERR_FILENO, "Error: Can't close fd%d\n", file_to);
 		exit(100);
 	}
 	return (0);
