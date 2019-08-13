500c3d10685e2ddb90f7d9113042afcc8c55d8ef
DNeosphere <daniela.gomezp05@outlook.com>
2019-08-13 23:16:51 +0000

    betty ok

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
 	}
 	file_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	if (file_to == -1)
-	{printf(STDERR_FILENO, "Error: Can't write to%s\n", argv[2]);
+	{dprintf(STDERR_FILENO, "Error: Can't write to%s\n", argv[2]);
 		exit(99);
 	}
 	size_write = write(file_to, buff, size_read);
