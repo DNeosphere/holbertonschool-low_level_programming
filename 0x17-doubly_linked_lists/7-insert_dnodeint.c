14aa0c8efb15bd7155bcd4e065339349c3efc7ff
DNeosphere <daniela.gomezp05@outlook.com>
2019-09-14 18:41:59 +0000

    Insert node at the end

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -30,7 +30,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		current = temp;
 		temp = temp->next;
 		if (temp == NULL)
-			return (NULL);
+			return (add_dnodeint_d(h, n));
 		count++;
 	}
 	current->next = new_node;
@@ -60,5 +60,37 @@ dlistint_t *add_dnodeint_d(dlistint_t **head,  int n)
 	if (*head != NULL)
 		(*head)->prev = new_node;
 	*head = new_node;
+	return (new_node);
+}
+/**
+ * add_dnodeint_end_d - adds a node at the end of a dlinked list
+ * @head: pointer to the first node of the list
+ * @n: number to fill the node
+ * Return: address of the new element or NULL if failed
+ */
+dlistint_t *add_dnodeint_end_d(dlistint_t **head, int n)
+{
+	dlistint_t *temp, *new_node;
+
+	temp = *head;
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (!*head)
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+		return (new_node);
+	}
+
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	new_node->prev = temp;
+	temp->next = new_node;
 	return (new_node);
 }
