cb9c4411585af54c32ecbe6554d6000f9c62f2dc
DNeosphere <daniela.gomezp05@outlook.com>
2019-09-14 18:30:21 +0000

    Task 7, insert node

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,64 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - insters a node in a given index
+ * @h: pointer to the first node
+ * @idx: index to insert the node
+ * @n: value to fill the node with
+ * Return: address of the new node;
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int count = 0;
+	dlistint_t *new_node, *temp = *h, *current;
+
+	if (h == NULL)
+		return (NULL);
+
+	if (idx == 0)
+	{
+		new_node = add_dnodeint_d(h, n);
+		return (new_node);
+	}
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = n;
+
+	while (count < idx)
+	{
+		current = temp;
+		temp = temp->next;
+		if (temp == NULL)
+			return (NULL);
+		count++;
+	}
+	current->next = new_node;
+	new_node->prev = temp;
+	new_node->next = temp->next;
+
+	return (new_node);
+}
+/**
+ * add_dnodeint_d - adds a node at the start of a dlinked list
+ * @head: pointer to the first node
+ * @n: number to be asigned to the node
+ * Return: address of the new node
+ */
+dlistint_t *add_dnodeint_d(dlistint_t **head,  int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
+
+	if (*head != NULL)
+		(*head)->prev = new_node;
+	*head = new_node;
+	return (new_node);
+}
