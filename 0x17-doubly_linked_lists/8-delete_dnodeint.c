962d23cceef2a282277cf65cf79fd9af9589106d
DNeosphere <daniela.gomezp05@outlook.com>
2019-09-14 20:21:14 +0000

    delete node at index, betty ok

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - frees memory from a given index
+ * @head: pointer to the first node
+ * @idx: index of the node to delete
+ * Return: 1 on success, else -1
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int idx)
+{
+	unsigned int count = 0;
+	dlistint_t *temp = *head, *next_add;
+
+	while (temp)
+	{
+		if (count == idx - 1)
+		{
+			next_add = (temp->next)->next;
+			free(temp->next);
+			temp->next = next_add;
+			temp->next->prev = temp;
+			return (1);
+		}
+		else if (idx == 0)
+		{
+			*head = (*head)->next;
+			if (temp->next != NULL)
+				temp->next->prev = NULL;
+			 free(temp);
+			return (1);
+		}
+		temp = temp->next;
+		count++;
+	}
+	return (-1);
+}
