d671614d93d48b9a92fe29f960c2427c77f66da2
DNeosphere <daniela.gomezp05@outlook.com>
2019-09-14 16:51:49 +0000

    Task 5, get the nth node of a dlinked list

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - finds the nth node of a dlinked list
+ * @head: pointer to the first node
+ * @index: of the node to be returned
+ * Return: address of the node or null if it doesn't exist
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t  *temp = head;
+	unsigned int count = 0;
+
+	if (head == NULL)
+		return (NULL);
+	while (count < index)
+	{
+		count++;
+		temp = temp->next;
+		if (temp == NULL)
+		{
+			printf("null");
+			return (NULL);
+		}
+	}
+	return (temp);
+}
