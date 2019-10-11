20b65950f17cecdf639f4f4dd304b8b93e8ee94e
DNeosphere <daniela.gomezp05@outlook.com>
2019-10-11 19:46:55 +0000

    Feature: has table existence verified

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int idx, size;
 	hash_node_t *n_node, *start, *current;
 
-	if (key == NULL || strlen(key) == 0)
+	if (key == NULL || strlen(key) == 0 || ht == NULL)
 		return (0);
 
 	size = ht->size;
