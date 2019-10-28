018f8efdaa7d37d5d26f605d661a245eb9038c18
DNeosphere <daniela.gomezp05@outlook.com>
2019-10-28 14:58:50 +0000

    Task 0

---
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+/**
+ * bubble_sort - function that sorts array of int
+ * with bubble method
+ * @array: list to be sorted
+ * @size: length og array inlet
+ */
+void bubble_sort(int *array, size_t size)
+{
+	unsigned int j = 0;
+	int aux = 0;
+
+
+	while (j < (size - 1))
+	{
+		if (array[j] > array[j + 1])
+		{
+			aux = array[j + 1];
+			array[j + 1] = array[j];
+			array[j] = aux;
+			j = 0;
+			print_array(array, size);
+			continue;
+		}
+		j++;
+	}
+
+}
