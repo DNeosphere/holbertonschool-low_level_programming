51852da33aff945c269eebc9815dfce027cebe0b
DNeosphere <daniela.gomezp05@outlook.com>
2019-10-28 16:26:43 +0000

    Fix: Two loops

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -7,24 +7,28 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	unsigned int j = 0;
+	unsigned int j = 0, i = 0;
 	int aux = 0;
 
 	if (size < 2 || !array)
 		return;
 
-	while (j <= size - 1)
+	while (i < size - 1)
 	{
-		if (array[j] > array[j + 1])
+		j =  0;
+		while (j < size - 1)
 		{
-			aux = array[j + 1];
-			array[j + 1] = array[j];
-			array[j] = aux;
-			j = 0;
-			print_array(array, size);
-			continue;
+			if (array[j] > array[j + 1])
+			{
+				aux = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = aux;
+				print_array(array, size);
+			}
+			j++;
 		}
-		j++;
+		i++;
 	}
+
 
 }
