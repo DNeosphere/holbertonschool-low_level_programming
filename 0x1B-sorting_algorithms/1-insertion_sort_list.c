97d4f8bb3b1abc079e8c3d9447844ea66fe5e70a
DNeosphere <daniela.gomezp05@outlook.com>
2019-10-28 15:07:37 +0000

    Insertion sort task

---
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - sorts a list using
+ * the insertion algorithm
+ * @list: double pointer to the list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *node;
+
+	node = (*list)->next;
+
+	while (node)
+	{
+		while ((node->prev != NULL) && (node->prev->n > node->n))
+		{
+			node->prev->next = node->next;
+			if (node->next)
+				node->next->prev = node->prev;
+
+			node->next = node->prev;
+			node->prev = node->prev->prev;  /* reverse update */
+			node->next->prev = node;
+
+			(node->prev == NULL) ? (*list = node) : (node->prev->next = node);
+			print_list(*list);
+		}
+
+		node = node->next;
+	}
+}
