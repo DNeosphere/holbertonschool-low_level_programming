842137fd2348fc5ade6776f50874e5aaaca49af4
DNeosphere <daniela.gomezp05@outlook.com>
2019-12-03 17:09:59 +0000

    Feature: task 7, inorder traverse

---
+++ 0x1D-binary_trees/7-binary_tree_inorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_inorder - goes through a binary tree using in-order traversal
+ * @tree: pointer to the root node of a b-tree
+ * @func: function pointer to print
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (func != NULL || tree != NULL)
+	{
+		if (tree == NULL)
+			return;
+
+		binary_tree_inorder(tree->left, func);
+		func(tree->n);
+		binary_tree_inorder(tree->right, func);
+
+	}
+}
