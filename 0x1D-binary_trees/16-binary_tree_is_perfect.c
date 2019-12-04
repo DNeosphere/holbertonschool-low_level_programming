befd83985691c3970d72dd620475679a9e5146b6
DNeosphere <daniela.gomezp05@outlook.com>
2019-12-04 19:37:21 +0000

    Feature: task 16, finds a perfect tree

--- 0x1D-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -17,8 +17,13 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	perfect = binary_tree_is_full(tree);
 
 	if (height_r == height_l && perfect == 1)
-		return (1);
+	{
+		if (binary_tree_is_perfect(tree->right) +
+		    binary_tree_is_perfect(tree->left) % 2 == 0)
+			return (1);
+	}
 	return (0);
+
 
 }
 /**
