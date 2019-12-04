550a827349f400e3a3a7c1c3e4122abcf0ee2f2d
DNeosphere <daniela.gomezp05@outlook.com>
2019-12-04 19:28:09 +0000

    Feature: task 16, finds a perfect tree

---
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -0,0 +1,71 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * @tree: pointer to the root
+ * Return: 1 if perfect, 0 otherwise
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int perfect = 0, height_l = 0, height_r = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	height_r = find_height(tree->right);
+	height_l = find_height(tree->left);
+
+	perfect = binary_tree_is_full(tree);
+
+	if (height_r == height_l && perfect == 1)
+		return (1);
+	return (0);
+
+}
+/**
+ * find_height - finds height
+ * @tree: pointer to the root of a tree
+ * Return: height of the tree
+ */
+int find_height(const binary_tree_t *tree)
+{
+
+	int l_height = 0;
+	int r_height = 0;
+
+	if (tree == NULL)
+		return (-1);
+
+	l_height = find_height(tree->left);
+	r_height = find_height(tree->right);
+
+	return ((l_height > r_height ? l_height : r_height) + 1);
+
+}
+/**
+ * binary_tree_is_full - Checks if a binary tree is full
+ * @tree: Binary tree
+ * Return: 0 if not full, 1 it is
+ */
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int left_t = 0, rigth_t = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	if (tree->left)
+		left_t = binary_tree_is_full(tree->left);
+	if (tree->right)
+		rigth_t = binary_tree_is_full(tree->right);
+
+	if (left_t == 0 && rigth_t == 0)
+		return (0);
+	else if ((left_t == 1 && rigth_t == 0) || (left_t == 0 && rigth_t == 1))
+		return (0);
+	else
+		return (1);
+}
