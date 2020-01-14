2034cf172e947955cfe89598adc5397cc6fbf3ec
DNeosphere <daniela.gomezp05@outlook.com>
2020-01-14 18:11:18 +0000

    Fix: add printf

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,17 +10,17 @@
 int linear_search(int *array, size_t size, int value)
 {
 
-    size_t i = 0;
+	size_t i = 0;
 
-    if (array == NULL)
-        return (-1);
+	if (array == NULL)
+		return (-1);
 
-    while (i < size)
-    {
-        if (aray[i] == value)
-            return (i);
-        i++;
-    }
-    if (i == size)
-        return (-1);
+	while (i < size)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+		i++;
+	}
+	return (-1);
 }
