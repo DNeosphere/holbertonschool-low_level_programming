b897b3cb2b6dedc0b85606814ff0e685ad0f3ccb
DNeosphere <daniela.gomezp05@outlook.com>
2020-01-15 18:41:10 +0000

    Fix: return -1 if array is null

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -11,6 +11,9 @@ int binary_search(int *array, size_t size, int value)
 {
 	int left = 0, right = size - 1, mid, i;
 
+	if (array == NULL
+	    return (-1);
+
 	while (left <= right)
 	{
 		printf("Searching in array: ");
