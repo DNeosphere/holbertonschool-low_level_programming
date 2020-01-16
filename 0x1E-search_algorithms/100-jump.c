99dbcba26b60c598786f192b5bb864bb585c5d6e
DNeosphere <daniela.gomezp05@outlook.com>
2020-01-16 22:59:03 +0000

    fix: betty

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -18,8 +18,8 @@ int jump_search(int *array, size_t size, int value)
 		printf("Value checked array[%ld] = [%d]\n", start, array[start]);
 		if (array[end] >= value || end >  size - 1)
 			break;
-        start = end;
-        end += sqt;
+		start = end;
+		end += sqt;
 	}
 	printf("Value found between indexes [%ld] and [%ld]\n", start, end);
 
@@ -27,7 +27,8 @@ int jump_search(int *array, size_t size, int value)
 	{
 		printf("Value checked array[%ld] = [%d]\n", start, array[start]);
 		if (start >= (size - 1))
-		    return (-1);
+			return (-1);
+
 		if (array[start] == value)
 			return (start);
 		start++;
