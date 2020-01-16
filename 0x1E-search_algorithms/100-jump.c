d8cc8781bfc4b45b08600ace05b15f9fe43cb427
DNeosphere <daniela.gomezp05@outlook.com>
2020-01-16 23:03:45 +0000

    fix: betty

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -21,7 +21,7 @@ int jump_search(int *array, size_t size, int value)
 	if (array[end] == value)
 		return (end);
 
-	while (array[start] < value)
+	while (array[start] <= value)
 	{
 		printf("Value checked array[%ld] = [%d]\n", start, array[start]);
 		if (array[end] >= value || end >  size - 1)
