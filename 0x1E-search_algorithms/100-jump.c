594b6f8eede6832820e3a07efef4ef67d3e2fa05
Dani Gomez <daniela.gomezp05@outlook.com>
2020-01-17 09:26:11 -0500

    Fix: return start

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -30,6 +30,8 @@ int jump_search(int *array, size_t size, int value)
 	while (array[start] < value)
 	{
 		printf("Value checked array[%ld] = [%d]\n", start, array[start]);
+		if (array[start] == value)
+			return (start);
 		if (array[end] >= value || end >  size - 1)
 			break;
 		start = end;
